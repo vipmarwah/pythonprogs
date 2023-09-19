427fb5d4e6aca36013c99073c4727e234080b787
vipmarwah <101658736+vipmarwah@users.noreply.github.com>
2023-09-19 14:44:13 -0600

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,15 +1,30 @@
-import os
-import sys
-from pyspark import SparkContext
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+from time import time
+import numpy as np
+from random import random
+from operator import add
 
-# Python3 program to add two numbers
-num1 = 15
-num2 = 12
- 
-# Adding two nos
-sum = num1 + num2
- 
-# printing values
-print("Sum of", num1, "and", num2 , "is", sum)
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('CalculatePi').getOrCreate()
+sc = spark.sparkContext
+
+n = 10000000
+
+def is_point_inside_unit_circle(p):
+    # p is useless here
+    x, y = random(), random()
+    return 1 if x*x + y*y < 1 else 0
+
+t_0 = time()
+
+# parallelize creates a spark Resilient Distributed Dataset (RDD)
+# its values are useless in this case
+# but allows us to distribute our calculation (inside function)
+count = sc.parallelize(range(0, n)) \
+             .map(is_point_inside_unit_circle).reduce(add)
+print(np.round(time()-t_0, 3), "seconds elapsed for spark approach and n=", n)
+print("Pi is roughly %f" % (4.0 * count / n))
+
+# VERY important to stop SparkSession
+# Otherwise, the job will keep running indefinitely
+spark.stop()
